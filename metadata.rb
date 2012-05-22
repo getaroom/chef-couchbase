71f42b40e216d70d0451958634ef9a40312bf41d
Development <dev@waldorf-astoria.local>
2012-05-22 18:08:00 -0500

    Declare logrotate cookbook dependency

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,8 @@ description      "Installs/Configures Couchbase"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.0"
 
+depends "logrotate"
+
 supports "debian"
 supports "ubuntu"
 
