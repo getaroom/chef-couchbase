721a092d0c46f90c2574f3ace66656d673ecfb87
Chris Griego and Morgan Nelson <pair+chris+morgan@roomvaluesteam.com>
2012-05-22 12:08:37 -0500

    Add database_path attribute to couchbase_node

--- libraries/node_resource.rb
+++ libraries/node_resource.rb
@@ -3,11 +3,14 @@ require "chef/resource"
 class Chef
   class Resource
     class CouchbaseNode < Resource
+      attribute :database_path, :kind_of => String, :default => "/opt/couchbase/var/lib/couchbase"
+
       def initialize(name, run_context=nil)
         super
-        @allowed_actions.push(:configure)
         @action = :configure
+        @allowed_actions.push(:configure)
         @provider = Provider::CouchbaseNode
+        @resource_name = :couchbase_node
       end
     end
   end
--- spec/node_resource_spec.rb
+++ spec/node_resource_spec.rb
@@ -21,6 +21,21 @@ describe Chef::Resource::CouchbaseNode do
     end
   end
 
+  describe "#database_path" do
+    it "can be assigned" do
+      resource.database_path "/mnt/couchbase-server/data"
+      resource.database_path.should == "/mnt/couchbase-server/data"
+    end
+
+    it "cannot be assigned an Integer" do
+      expect { resource.database_path 42 }.to raise_error Chef::Exceptions::ValidationFailed
+    end
+
+    it "defaults to /opt/couchbase/var/lib/couchbase" do
+      resource.database_path.should == "/opt/couchbase/var/lib/couchbase"
+    end
+  end
+
   describe "#name" do
     it "assigns the given name attribute" do
       described_class.new("self").name.should == "self"
@@ -31,5 +46,10 @@ describe Chef::Resource::CouchbaseNode do
     it "defaults to the CouchbaseNode provider class" do
       resource.provider.should == Chef::Provider::CouchbaseNode
     end
+  end
+
+  describe "#resource_name" do
+    subject { resource.resource_name }
+    it { should == :couchbase_node }
   end
 end
