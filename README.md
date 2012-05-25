43615cc639ae7b947cbb5d8b60f087732106155b
Chris Griego <cgriego@gmail.com>
2012-05-24 23:42:37 -0500

    Extracted package file/url variables to attributes

--- README.md
+++ README.md
@@ -16,6 +16,9 @@ ATTRIBUTES
 
 * `node['couchbase']['edition']`         - The edition of Couchbase to install, "community" or "enterprise"
 * `node['couchbase']['version']`         - The version of Couchbase to install
+* `node['couchbase']['package_file]`     - The package file to download and install
+* `node['couchbase']['package_base_url]` - The url path to download the package file from
+* `node['couchbase']['package_full_url]` - The full url to the package file to download and install
 * `node['couchbase']['database_path']`   - The directory Couchbase should persist data to
 * `node['couchbase']['log_dir']`         - The directory Couchbase should log to
 * `node['couchbase']['memory_quota_mb']` - The per server RAM quota for the entire cluster in megabytes
--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,18 @@
+package_machine = node['kernel']['machine'] == "x86_64" ? "x86_64" : "x86"
+
+case node['platform']
+when "debian", "ubuntu"
+  package_ext = "deb"
+when "redhat", "centos", "scientific", "amazon"
+  package_ext = "rpm"
+end
+
 default['couchbase']['edition'] = "community"
 default['couchbase']['version'] = "1.8.0"
+
+default['couchbase']['package_file'] = "couchbase-server-#{node['couchbase']['edition']}_#{package_machine}_#{node['couchbase']['version']}.#{package_ext}"
+default['couchbase']['package_base_url'] = "http://packages.couchbase.com/releases/#{node['couchbase']['version']}"
+default['couchbase']['package_full_url'] = "#{node['couchbase']['package_base_url']}/#{node['couchbase']['package_file']}"
 
 default['couchbase']['database_path'] = "/opt/couchbase/var/lib/couchbase/data"
 default['couchbase']['log_dir'] = "/opt/couchbase/var/lib/couchbase/logs"
