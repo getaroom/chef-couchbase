e436cd077bb5bf799b46b755868c03236ac85bd7
Chris Griego <cgriego@gmail.com>
2012-05-26 00:31:09 -0500

    Fix usage of credentials when reading settings data from Couchbase API
    Log error message from Couchbase when posting settings

--- libraries/settings_provider.rb
+++ libraries/settings_provider.rb
@@ -4,17 +4,18 @@ class Chef
   class Provider
     class CouchbaseSettings < Provider
       def load_current_resource
-        @current_resource = Resource::CouchbaseSettings.new(@new_resource.name)
+        @current_resource = Resource::CouchbaseSettings.new @new_resource.name
         @current_resource.group @new_resource.group
         @current_resource.settings settings_data
       end
 
       def action_modify
         unless settings_match?
-          uri = URI("http://#{host}:8091/settings/#{@new_resource.group}")
-          Net::HTTP.post_form(uri, @new_resource.settings).value
-          @new_resource.updated_by_last_action(true)
-          Chef::Log.info("#{@new_resource} modified")
+          response = Net::HTTP.post_form(uri, @new_resource.settings)
+          Chef::Log.error response.body unless response.kind_of? Net::HTTPSuccess
+          response.value
+          @new_resource.updated_by_last_action true
+          Chef::Log.info "#{@new_resource} modified"
         end
       end
 
@@ -24,15 +25,18 @@ class Chef
         @new_resource.settings.all? { |key, value| @current_resource.settings[key.to_s] == value }
       end
 
-      def host
-        "#{@new_resource.username}:#{@new_resource.password}@localhost"
+      def uri
+        @uri ||= URI.parse "http://#{@new_resource.username}:#{@new_resource.password}@localhost:8091/settings/#{@new_resource.group}"
       end
 
       def settings_data
-        return @settings_data if instance_variable_defined? "@settings_data"
-
         @settings_data ||= begin
-          response = Net::HTTP.get_response(host, "/settings/#{@new_resource.group}", 8091)
+          response = Net::HTTP.start(uri.host, uri.port) do |http|
+            request = Net::HTTP::Get.new uri.path
+            request.basic_auth uri.user, uri.password
+            http.request request
+          end
+
           response.value
           JSONCompat.from_json response.body
         end
