b22ef00fc28db686d3225af33333f58f916d2c92
Chris Armstrong <chris.armstrong@socrata.com>
2013-12-18 15:40:54 -0800

    Don't bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "jdunn@opscode.com"
 license          "MIT"
 description      "Installs and configures Couchbase Server."
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version          "1.2.0"
+version          "1.1.0"
 
 %w{debian ubuntu centos redhat oracle amazon scientific windows}.each do |os|
   supports os
